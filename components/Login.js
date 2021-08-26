3928dbb7f66f9546fd85a1a72199fa6a9457c61a
Yuki Li <yunqili1025@gmail.com>
2021-08-26 10:36:29 -0400

    touchup for initial demo

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { Text, View, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 const Login = ({onClose}) => {
   const [text, onChangeText] = React.useState("User Name");
+  const [records, setRecords] = React.useState('');
+  const [showRecords, setShowRecords] = React.useState(false);
+  const getRecords = () => {
+    setShowRecords(true);
+    axios.get('http://localhost:3000/records')
+    .then((result) => {
+      setRecords(result.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  };
 
   return (
-    <View>
+    <View style={{alignItems: 'center'}}>
+      <Image
+        style={styles.logo}
+        source={{
+          // uri: 'https://cdn.iconscout.com/icon/premium/png-256-thumb/snake-game-1513688-1282916.png',
+          uri: 'https://cdn.iconscout.com/icon/premium/png-256-thumb/snake-game-1513124-1282989.png',
+        }}
+      />
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
@@ -16,8 +36,15 @@ const Login = ({onClose}) => {
         style={styles.button}
         onPress={onClose}
       >
-        <Text>start!</Text>
+        <Text>Play!</Text>
       </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={getRecords}
+      >
+        <Text>Check score board</Text>
+      </TouchableOpacity>
+      {showRecords && <Text style={styles.records}>{records}</Text>}
     </View>
   )
 };
@@ -35,6 +62,16 @@ const styles = StyleSheet.create({
     width: 200,
     alignItems: "center",
     backgroundColor: "#DDDDDD",
+    padding: 10,
+    margin: 12,
+  },
+  records: {
+    backgroundColor: "#DDDDDD",
+  },
+
+  logo: {
+    width: 126,
+    height: 118,
     padding: 10,
     margin: 12,
   },
